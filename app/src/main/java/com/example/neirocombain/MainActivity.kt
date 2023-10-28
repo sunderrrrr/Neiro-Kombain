99b2095cbf6f180fe0ac004e241d7a719dd521b4
Ilya Korotaev <101642629+Bkmz7692@users.noreply.github.com>
2023-10-28 11:39:21 +0400

    repair

--- app/src/main/java/com/example/neirocombain/MainActivity.kt
+++ app/src/main/java/com/example/neirocombain/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.neirocombain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+//import okhttp3.*
+//import okhttp3.MediaType.Companion.toMediaTypeOrNull
+//import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    //private val client = OkHttpClient()
+    // creating variables on below line.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+
 }
