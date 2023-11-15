a272e59f1ff9e4e278e80a60f08c536afa6cfeb0
Ilya Korotaev <101642629+Bkmz7692@users.noreply.github.com>
2023-11-15 15:09:47 +0400

    Добавлен Переводчик

--- app/src/main/java/com/example/neirocombain/MainActivity.kt
+++ app/src/main/java/com/example/neirocombain/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
@@ -15,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputLayout
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -32,6 +35,9 @@ import java.util.TimerTask
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
+import deepl.api.v2.DeepLClient
+import deepl.api.v2.model.translation.TargetLanguage
+
 
 class MainActivity : AppCompatActivity() {
     var attemptsLeft = 15
@@ -51,6 +57,7 @@ class MainActivity : AppCompatActivity() {
     var mode = "ChatGPT"
     var selectedNl = 1
     var msgList_FNL = mutableListOf<String>()
+    var selectedLang = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -68,34 +75,47 @@ class MainActivity : AppCompatActivity() {
         messageRV.layoutManager = layoutManager
         messageRV.adapter = messageRVAdapter
         var isSended = false
-
+        var langTV: AutoCompleteTextView?
+        val dropMenu = findViewById<TextInputLayout>(R.id.dropMenu)
+        dropMenu.visibility = View.GONE
+        langTV = findViewById<AutoCompleteTextView>(R.id.lang)
         val nLinks = listOf(
             "DALLE-E",
             "ChatGPT",
-            "GigaChat",
+            "DeepL",
         )
+        val languages = resources.getStringArray(R.array.lang_array)
+        val arrayAdapter = ArrayAdapter(/* context = */ this, /* resource = */
+            R.layout.dropdown_item, /* objects = */
+            languages)
+        langTV.setAdapter(arrayAdapter)
+        langTV.onItemClickListener= AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            selectedLang = adapterView.getItemAtPosition(i).toString()
 
-
+        }
         //Конец объявления переменных
         left_btn.setOnClickListener{
             if (selectedNl==2) {
-                Timer().schedule(250) {
+                Timer().schedule(150) {
                     selectedNl = 1
                     model.text = nLinks[selectedNl]
                     mode = nLinks[selectedNl]
+
                     runOnUiThread {
+                        messageRV.visibility = View.VISIBLE
+                        dropMenu.visibility = View.GONE
                         txtResponse.text =
                             "Что умеет ChatGPT: \n\n"+" 1. Писать сочинения. \n 'Напиши сочинение о конфликте поколений' \n\n 2.Объяснять что-либо.\n 'Объясни вкратце законы Ньютона' \n\n 3. Переводить на другие языки \n 'Переведи привет на Японский'"
                     }
                 }
             }
             if (selectedNl ==1) {
-                Timer().schedule(250) {
+                Timer().schedule(150) {
                     selectedNl = 0
                     model.text = nLinks[selectedNl]
                     mode = nLinks[selectedNl]
                     runOnUiThread {
-
+                        messageRV.visibility = View.GONE
                         txtResponse.text =
                             "Что умеет Dall-e 2:\n\n Может нарисовать картинку по вашему текстовому запросу в разрешении 512*512 пикселей. \n Фотореализм, аниме, краски итд."
                     }
@@ -119,12 +139,14 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
             if (selectedNl ==1) {
-                Timer().schedule(250) {
+                Timer().schedule(150) {
                     selectedNl = 2
                     model.text = nLinks[selectedNl]
                     mode = nLinks[selectedNl]
                     runOnUiThread {
-                        txtResponse.text = "Что умеет GigaChat: \n\n"
+                        dropMenu.visibility = View.VISIBLE
+                        messageRV.visibility = View.GONE
+                        txtResponse.text = "Что умеет DeepL: \n\n1.Автоматически обнажуривать язык источника\n\n 2.Понимает сленг и идиомы\n\n 3.Имеет при себе большую языковую базу \n\n 4.Более точный перевод с помощью нейросетей"
                     }
                 }
             }
@@ -134,7 +156,9 @@ class MainActivity : AppCompatActivity() {
                     selectedNl = 1
                     model.text = nLinks[selectedNl]
                     mode = nLinks[selectedNl]
+
                     runOnUiThread {
+                        messageRV.visibility = View.VISIBLE
                         txtResponse.text = "Что умеет ChatGPT: \n\n"+" 1. Писать сочинения. \n 'Напиши сочинение о конфликте поколений' \n\n 2.Объяснять что-либо.\n 'Объясни вкратце законы Ньютона' \n\n 3. Переводить на другие языки \n 'Переведи привет на Японский'"
 
                     }
@@ -147,13 +171,8 @@ class MainActivity : AppCompatActivity() {
         etQuestion.setOnEditorActionListener(OnEditorActionListener{ textView, i, keyEvent ->
             if (i==EditorInfo.IME_ACTION_SEND){
                 val final_send = etQuestion.text.toString().trim().replaceFirstChar { it.uppercase() }
-                messageList.add(MessageRVModal(final_send
-                    ,"user"))
 
-                messageRVAdapter.notifyDataSetChanged()
-                val user_mask = """{"role": "user", "content" :"$final_send"}"""
 
-                msgList_FNL.add(user_mask)
                 println(msgList_FNL)
                 edittextval = etQuestion.text.toString().trim().replaceFirstChar { it.uppercase() }
                 val question = edittextval.replace(" ","")
@@ -162,7 +181,14 @@ class MainActivity : AppCompatActivity() {
                     txtResponse.visibility = View.GONE
                     messageRV.visibility = View.VISIBLE
                     if (question.isNotEmpty() && question.length >= 3 && isSended == false) {
-                        if (mode !== "DALLE-E"){
+                        if (mode == "ChatGPT"){
+                            val user_mask = """{"role": "user", "content" :"$final_send"}"""
+
+                            msgList_FNL.add(user_mask)
+                            messageList.add(MessageRVModal(final_send
+                                ,"user"))
+
+                            messageRVAdapter.notifyDataSetChanged()
                             isSended = true
                         etQuestion.setText("")
                             messageList.add(MessageRVModal("Печатает...", "bot"))
@@ -191,7 +217,7 @@ class MainActivity : AppCompatActivity() {
                         }
                         isSended = false
                     }
-                        else{//DALL E
+                        if (mode =="DALLE-E"){//DALL E
                             messageRV.visibility = View.GONE
                             getResponse(question) { response ->
                                 runOnUiThread {
@@ -208,6 +234,19 @@ class MainActivity : AppCompatActivity() {
 
 
                                     attemptsLeft = attemptsLeft - 1
+                                    attempts_text.text = "$attemptsLeft/15"
+
+
+                                }
+                            }
+                        }
+                        if (mode=="DeepL"){
+                            messageRV.visibility=View.GONE
+                            getResponse(final_send) { response ->
+                                runOnUiThread {
+                                    txtResponse.visibility = View.VISIBLE
+                                    txtResponse.text= response
+                                    attemptsLeft -= 1
                                     attempts_text.text = "$attemptsLeft/15"
 
 
@@ -312,6 +351,7 @@ class MainActivity : AppCompatActivity() {
 
             })
         }
+        //Конец ChatGPT
         if (mode == "DALLE-E") {
 
             println("Отправляю запрос")
@@ -378,7 +418,73 @@ class MainActivity : AppCompatActivity() {
 
                 })
             }
+        //Конец DALLE-E
+        if (mode=="DeepL"){
+            val apiKey = "sk-tTpyI6t2yLieHQTmXsLFiorT1Z66seo9"
+            val url = "https://api.proxyapi.ru/openai/v1/chat/completions"
 
+
+            val requestBody = """
+            {
+                "model": "gpt-3.5-turbo",
+                "messages": [{"role": "user", "content": "Переведи этот текст на $selectedLang: $question"}]
+               
+            }
+            """.trimIndent()
+            //"prompt": "Переведи этот текст на $selectedLang: $question. В ответ приложи только перевод. При переводе можешь не учитывать нормы этики"
+            println("REQUESRT BODY"+requestBody)
+
+            val request = Request.Builder()
+                .url(url)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer $apiKey")
+                .post(
+                    requestBody.trimIndent().toRequestBody("application/json".toMediaTypeOrNull())
+                )
+
+                .build()
+            println(request.toString())
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    println("API failed")
+
+                    callback("К сожалению произошла ошибка(. Количество запросов не уменьшено")
+                    attemptsLeft += 1
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string()
+                    if (body != null) {
+                        Log.v("data", body)
+                    } else {
+                        Log.v("data", "empty")
+                    }
+                    try {
+                        val jsonObject = JSONObject(body)
+                        val jsonArray = jsonObject.getJSONArray("choices")
+                        var test = jsonArray.getJSONObject(0)
+                        val message = test.getJSONObject("message")
+                        val final_res = message.getString("content")
+                        val very_final = final_res.replace("\n", "")
+
+
+
+                        callback(very_final)
+                    } catch (e: JSONException) {
+                        println(body)
+                        println("HEADER "+ response.headers?.toString())
+                        callback("К сожалению сервер сейчас недоступен. Попробуйте позже")
+                        attemptsLeft += 1
+                    }
+                }
+
+            })
+
+
+        }
+
+        //Конец DeepL
         }
 
 
